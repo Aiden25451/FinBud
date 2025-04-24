9cf66e356a66d215181aca18f20b81393678b2b1
Aiden Asprakis <aasprakis@Aidens-MacBook-Air.local>
2025-04-23 20:37:38 -0400

    Testing stuff out 3

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,6 +1,3 @@
-'use client';
-
-import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { TypeAnimation } from 'react-type-animation';
@@ -9,70 +6,25 @@ import Button from '@/_components/Button-Component/Button';
 import { redirect } from 'next/navigation';
 
 const HomePage: React.FC = () => {
-  const router = useRouter();
-  const [loading, setLoading] = useState(true);
-
-  function redirectAfterLogin() {
-    if (sessionStorage?.getItem('url')) {
-      var redirectURL = sessionStorage.getItem('url').toString();
-      sessionStorage.removeItem('url');
-      redirect(redirectURL);
-    }
-  }
-
-  useEffect(() => {
-    redirectAfterLogin();
-  });
-
   return (
     <>
-      <div className="h-screen flex items-center justify-center">
-        {/* Parent container for image and text */}
-        <div
-          className={`flex flex-col-reverse sm:flex-row items-center ${
-            loading ? 'hidden' : '' // If isLoading, hide everything, else show loading screen
-          }`}
-        >
-          {/* Image container */}
-          <div className="sm:w-1/3 flex justify-center sm:justify-start sm:mt-64 pt-10">
-            <Image
-              src="/images/Fin.webp"
-              alt="Logo"
-              width={300}
-              height={300}
-              className="w-auto h-auto"
-              priority
-              unoptimized
-              onLoadingComplete={() => setLoading(false)}
-            />
-          </div>
+      {/* Image container */}
+      <div className="sm:w-1/3 flex justify-center sm:justify-start sm:mt-64 pt-10">
+        <Image
+          src="/images/Fin.webp"
+          alt="Logo"
+          width={300}
+          height={300}
+          className="w-auto h-auto"
+          priority
+          unoptimized
+        />
+      </div>
 
-          {/* Text Area */}
-          <div className="sm:w-2/3 sm:mr-10 text-left rounded-xl mt-12">
-            <Textbox
-              label={
-                <TypeAnimation
-                  sequence={[
-                    'Hi! I’m Fin, your Virtual Finance Buddy. I’m here to guide you through every stage of your personal finance journey.',
-                    1000,
-                  ]}
-                  wrapper="p"
-                  speed={75}
-                  cursor={false}
-                  repeat={0}
-                  preRenderFirstString={false}
-                />
-              }
-              chatBubble
-            />
-
-            <div className="mt-4 flex justify-center">
-              <Button
-                label="Start My Journey"
-                onClick={() => router.push('/disclaimer')}
-              />
-            </div>
-          </div>
+      {/* Text Area */}
+      <div className="sm:w-2/3 sm:mr-10 text-left text-white rounded-xl mt-12">
+        <div className="mt-4 flex justify-center">
+          GoodBye
         </div>
       </div>
     </>
--- apps/frontend/src/app/quiz/page.tsx
+++ apps/frontend/src/app/quiz/page.tsx
@@ -1,23 +1,29 @@
+import Loading from '@/_components/loader/loading';
 import QuizPage from '@/_components/Quiz-Component/quiz';
 import { getHistoryFunction } from '@/_utils/quiz-functions';
 import { getSession } from '@auth0/nextjs-auth0';
+import { Suspense } from 'react';
 
 export default async function Page() {
   const session = await getSession();
 
   if (session) {
     const data = await getHistoryFunction();
-    return <QuizPage data={data}></QuizPage>;
+    return <Suspense fallback={<Loading />}>
+      <QuizPage data={data}></QuizPage>;
+    </Suspense>
   }
 
   return (
-    <QuizPage
-      data={{
-        loading: false,
-        historyArray: [0],
-        error: 'Not logged in',
-        initialState: false,
-      }}
-    ></QuizPage>
+    <Suspense fallback={<Loading />}>
+      <QuizPage
+        data={{
+          loading: false,
+          historyArray: [0],
+          error: 'Not logged in',
+          initialState: false,
+        }}
+      ></QuizPage>
+    </Suspense>
   );
 }
