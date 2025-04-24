610dba7bb3dff7aa6ac33addbb468ac9ed7c2451
Aiden Asprakis <aasprakis@Aidens-MacBook-Air.local>
2025-04-23 20:39:30 -0400

    Testing stuff out 4

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,9 +1,6 @@
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
-import { TypeAnimation } from 'react-type-animation';
 import Textbox from '@/_components/Textbox-Component/Textbox';
 import Button from '@/_components/Button-Component/Button';
-import { redirect } from 'next/navigation';
 
 const HomePage: React.FC = () => {
   return (
